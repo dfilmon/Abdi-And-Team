710dbead6fd0323166b43b81754f596cc588176c
dfilmon <abraha.filmon@gmail.com>
2022-12-06 20:26:51 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Abdi-And-Team
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-zmhaqn)
---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import 'tailwindcss/tailwind.css';
+import Nav from './Nav';
+import Hero from './Hero';
+
+export default function App() {
+  return (
+    <div>
+      <Nav />
+      <Hero />
+    </div>
+  );
+}
